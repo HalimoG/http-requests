b4674b867fb30e7154997de42a0b09efbde08f06
Halimo <halimoghelle@gmail.com>
2019-02-11 16:06:26 +0000

    step1

---
+++ step1.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getAndPrintHTMLCHUNCKS(){
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+      };
+
+      https.get(requestOptions, function (response){
+         
+        response.setEncoding('utf8');
+
+        response.on('data', function (data) {
+            console.log(data + "\n");
+          });
+        response.on('end', function() {
+            console.log('Response stream complete.');
+          });
+
+      });
+    }
+
+    getAndPrintHTMLCHUNCKS();
+
